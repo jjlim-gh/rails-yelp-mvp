4555c880d562c7b0d4d20ed300b9dee3227bf880
Justin Limlengco <jjmlimlengco@gmail.com>
2024-01-09 22:05:42 +0900

    Completed view and controller for GET #index, working on GET for #new

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,12 @@ Restaurant.destroy_all
 
 puts "Creating restaurants..."
 all_resto = []
-all_resto << {name: "Sukiya", address: "Meguro", category: "Japanese"}
-all_resto << {name: "Saizeriya", address: "Ishige", category: "Italian"}
-all_resto << {name: "Tim Ho Wan", address: "Kasuga", category: "Chinese"}
-all_resto << {name: "Ichiran", address: "Shibuya", category: "Japanese"}
-all_resto << {name: "St. Pierre", address: "Harajuku", category: "French"}
-all_resto << {name: "Manneken", address: "Kumamoto", category: "Belgian"}
+all_resto << {name: "Sukiya", address: "Meguro", phone_number: "080-1231-231", category: "Japanese"}
+all_resto << {name: "Saizeriya", address: "Ishige", phone_number: "080-1234-567", category: "Italian"}
+all_resto << {name: "Tim Ho Wan", address: "Kasuga", phone_number: "080-7654-321", category: "Chinese"}
+all_resto << {name: "Ichiran", address: "Shibuya", phone_number: "N/A", category: "Japanese"}
+all_resto << {name: "St. Pierre", address: "Harajuku", phone_number: "070-3344-789", category: "French"}
+all_resto << {name: "Manneken", address: "Kumamoto", phone_number: "080-6969-531", category: "Belgian"}
 
 
 all_resto.each do |attributes|
