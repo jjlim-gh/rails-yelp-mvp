3746dab25252667103e352a63f3808fd700e446b
Justin Limlengco <jjmlimlengco@gmail.com>
2024-01-09 20:57:35 +0900

    Added validations for the restaurant and review model, created data and seeded the DB

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(Chinese Italian Japanese French Belgian) }
+  has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+all_resto = []
+all_resto << {name: "Sukiya", address: "Meguro", category: "Japanese"}
+all_resto << {name: "Saizeriya", address: "Ishige", category: "Italian"}
+all_resto << {name: "Tim Ho Wan", address: "Kasuga", category: "Chinese"}
+all_resto << {name: "Ichiran", address: "Shibuya", category: "Japanese"}
+all_resto << {name: "St. Pierre", address: "Harajuku", category: "French"}
+all_resto << {name: "Manneken", address: "Kumamoto", category: "Belgian"}
+
+
+all_resto.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
