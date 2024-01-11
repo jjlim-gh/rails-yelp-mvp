819c95b93eceb2ae73dc4e70d7120b3306f8cb28
Justin Limlengco <jjmlimlengco@gmail.com>
2024-01-11 21:12:35 +0900

    Able to add show and new(& create) actions with the corresponding views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,27 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    restaurant_selector
+  end
+
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def restaurant_selector
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, :address, :category, presence: true
+  validates :name, :address, :category, presence: { message: "Please type a name..." }
   validates :category, inclusion: { in: %w(Chinese Italian Japanese French Belgian) }
   has_many :reviews, dependent: :destroy
 end
